438e22d9d156fda5f81c3875e23376950ac2edd6
mja00 <teamnirvana2@gmail.com>
2025-01-17 12:14:12 -0500

    feat: initial mod

---
+++ common/src/main/java/dev/mja00/commandexecutionwho/mixin/MixinItemStack.java
@@ -0,0 +1,21 @@
+package dev.mja00.commandexecutionwho.mixin;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(ItemStack.class)
+public class MixinItemStack {
+
+    @Shadow
+    private static final List<Component> OP_NBT_WARNING = List.of();
+}
